4e80533a2bbf0816b729d6c7c7088ea349dd1fd7
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-02-17 02:43:36 +0300

    feat: change body background image by button click

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const quoteReloadBtn = document.querySelector('.quote__reload-btn');
 quoteReloadBtn.addEventListener('click', getData);
 
 async function getData() {
+  const COUNT_BG_IMAGES = 10;
+
+  const getBackgroundImageIndex = () => {
+    return Math.floor((Math.random() * COUNT_BG_IMAGES) + 1);
+  }
+
+  const img = new Image();
+  img.src = `./assets/img/bg-${getBackgroundImageIndex()}.jpg`;
+
   const res = await fetch(url);
   const data = await res.json();
+
+  data['img'] = img.src;
+
   showData(data);
 }
 
 function showData(data) {
+  document.body.style.backgroundImage = `url('${data['img']}')`;
   quoteText.textContent = data.quote.body;
   quoteAuthor.textContent = data.quote.author;
 }
